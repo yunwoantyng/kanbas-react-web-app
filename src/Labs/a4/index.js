c24ff69cdb7a977d84a5261d09ad23f542b69066
雲婉庭 <yunwoantyng@yunwantingdeiMac.local>
2023-11-02 00:46:40 -0400

    lab first part

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -1,8 +1,38 @@
+import Add from "./Add";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+
+
 function Assignment4() {
+    const sayHello = () => {
+        alert("Hello World");
+    };
     return (
-        <div>
+      <div>
         <h1>Assignment 4</h1>
-        </div>
+        <Add a={1} b={2} />
+        <ClickEvent />
+        <PassingDataOnEvent />
+        <PassingFunctions theFunction={sayHello}/>
+        <EventObject />
+        <Counter />
+        <BooleanStateVariables />
+        <StringStateVariables />
+        <DateStateVariable />
+        <ObjectStateVariable />
+        <ArrayStateVariable />
+        <ParentStateComponent />
+
+      </div>
     );
 }
 export default Assignment4;
