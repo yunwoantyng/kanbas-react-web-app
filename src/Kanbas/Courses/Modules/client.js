1c4a8f3eec51aba7de15b61beaeef3433e23788b
雲婉庭 <yunwoantyng@yunwantingdeiMac.local>
2023-11-19 20:19:04 -0500

    heroku

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+// const MODULES_URL = "http://localhost:4000/api/modules";
+const MODULES_URL =
+  "https://kanbas-node-server-app-yun-6ce074579da2.herokuapp.com/api/modules";
+
 export const updateModule = async (module) => {
   const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
   return response.data;
